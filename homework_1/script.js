cb54afe7d82fc0f62b2c8ddfd9d86db7b9bbc564
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2021-01-12 18:46:20 +0500

    Задание выполнено

--- homework_1/script.js
+++ homework_1/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let money = +prompt('Ваш бюджет на месяц?',''),
+    time = prompt("Введите дату в формате YYYY-MM-DD"),
+    appData = {
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false,
+    };
+
+for( let i = 0; i < 2; i++) {
+    let ans1 = prompt('Введите обязательную статью расходов в этом месяце',''),
+        ans2 = +prompt('Во сколько обойдется?','');
+
+    appData.expenses[ans1] = ans2;
+}
+
+alert('Ваш ежедневный бюджет: ' + (appData.budget / 30) + " руб.")
