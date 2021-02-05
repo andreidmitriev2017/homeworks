0b09955f528b651592d1b77311a73b70d78f959d
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2021-02-05 13:29:42 +0500

    Добавлен таймер обратного отсчета

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -34,5 +34,58 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
-    })
+    });
+
+    //Timer
+    let deadline = '2021-02-05T13:21';
+    
+    function getTimeRemaining(endtime) {
+
+        let t = Date.parse(endtime) - Date.now();
+
+        if (t < 0) {
+            return {
+                'total': "00",
+                'hours': "00",
+                'minutes': "00",
+                'seconds': "00",
+            };
+        }
+
+        let seconds = Math.floor( (t / 1000) % 60 ),
+            minutes = Math.floor( (t / 1000 / 60) % 60 ),
+            hours = Math.floor( t / (1000 * 60 * 60) );
+
+        if (seconds < 10) seconds = '0' + seconds;
+        if (minutes < 10) minutes = '0' + minutes;
+        if (hours < 10) hours = '0' + hours;
+        
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
