6a154b79bf576883f56f8ea970536cdc7250327c
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2021-02-07 12:19:24 +0500

    Задача 11.2. Решена задача привязывания контекста

---
+++ task_1.1/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let age = document.getElementById('age');
+
+function showUser(surname, name) {
+  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+let newFunc = showUser.bind(age);
+
+newFunc("Иванов", "Иван");
