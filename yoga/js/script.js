67a5b8807f4affd09b7defb2070c1c654972af70
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2021-02-13 02:18:55 +0500

    Задание 13. Реализован скрипт отправки данных из формы

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -129,4 +129,97 @@ window.addEventListener('DOMContentLoaded', function() {
             } 
         });
     });
+
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с Вами свяжемся!',
+        failure: 'Что-то пошло не так...',
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', '../server.php');
+
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        console.log(obj);
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+               statusMessage.innerHTML = message.success; 
+            } else {
+                statusMessage.innerHTML = message.failure; 
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+    let contactForm = document.querySelector('#form'),
+        contactInputs = contactForm.getElementsByTagName('input'),
+        contactMessage = document.createElement('div');
+
+    contactMessage.classList.add('status');
+
+    contactForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        contactForm.appendChild(contactMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', '../server.php');
+
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(contactForm);
+
+        let obj = {};
+
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4){
+                contactMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                contactMessage.innerHTML = message.success;
+            } else {
+                contactMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let input of contactInputs) {
+            input.value = '';
+        }
+    });
 });
